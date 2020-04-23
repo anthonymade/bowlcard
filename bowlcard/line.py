3b49be19950e8ec2ef284f8172bd02d908131a65
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 19:30:31 +0100

    refactor to remove duplicate logic for bowl validation

--- bowlcard/line.py
+++ bowlcard/line.py
@@ -26,11 +26,13 @@ class Line:
         return score
 
     def score_frame(self, bowl1: int, bowl2: int) -> None:
-        if bowl1 > 10:
-            raise Exception(f"bowl 1 cannot be greater than 10, it was {bowl1}")
-        if bowl2 > 10:
-            raise Exception(f"bowl 2 cannot be greater than 10, it was {bowl2}")
+        self.__validate_bowl_value(1, bowl1)
+        self.__validate_bowl_value(2, bowl2)
         self.__frames.append(Frame(bowl1, bowl2))
+
+    def __validate_bowl_value(self, bowl_number: int, bowl_value: int) -> None:
+        if bowl_value > 10:
+            raise Exception(f"bowl {bowl_number} cannot be greater than 10, it was {bowl_value}")
 
     def get_frame(self, frame_number: int):
         return self.__frames[frame_number -1]

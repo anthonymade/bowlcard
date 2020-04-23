21260d659c28f4879845aa176a5aa6b442fca51a
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 19:27:03 +0100

    pass tests

--- bowlcard/line.py
+++ bowlcard/line.py
@@ -28,6 +28,8 @@ class Line:
     def score_frame(self, bowl1: int, bowl2: int) -> None:
         if bowl1 > 10:
             raise Exception(f"bowl 1 cannot be greater than 10, it was {bowl1}")
+        if bowl2 > 10:
+            raise Exception(f"bowl 2 cannot be greater than 10, it was {bowl2}")
         self.__frames.append(Frame(bowl1, bowl2))
 
     def get_frame(self, frame_number: int):

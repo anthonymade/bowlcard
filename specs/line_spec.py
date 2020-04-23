e2aa7609a37402da83f9078eb5130a80eeaf3d2e
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 17:54:10 +0100

    remove redundant tests

--- specs/line_spec.py
+++ specs/line_spec.py
@@ -10,41 +10,6 @@ class LineSpec(Specification):
             Line().get_score() == 0
 
 
-    def we_can_score_a_frame(self):
-
-        with given:
-            line = Line()
-
-        with when:
-            line.score_frame(bowl1, bowl2)
-
-        with then:
-            line.get_score() == expected_score
-
-        with where:
-            bowl1 = [3]
-            bowl2 = [4]
-            expected_score = [7]
-
-
-    def we_can_get_the_individual_scores_for_a_frame(self):
-
-        with given:
-            line = Line()
-
-        with when:
-            line.score_frame(bowl1, bowl2)
-
-        with then:
-            frame_score = line.get_frame(1)
-            frame_score.bowl1 == bowl1
-            frame_score.bowl2 == bowl2
-
-        with where:
-            bowl1 = [2]
-            bowl2 = [1]
-
-
     def we_can_score_multiple_frames(self):
 
         with given:

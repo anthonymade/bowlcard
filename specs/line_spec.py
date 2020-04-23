830e464e06fd1c3351ac130ec9d41013cff26d3c
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 17:02:59 +0100

    add failing score frame spec

--- specs/line_spec.py
+++ specs/line_spec.py
@@ -9,3 +9,19 @@ class LineSpec(Specification):
         with expect:
             Line().get_score() == 0
 
+
+    def we_can_score_a_frame(self):
+
+        with given:
+            line = Line()
+
+        with when:
+            line.score_frame(bowl1, bowl2)
+
+        with then:
+            line.get_score() == expected_score
+
+        with where:
+            bowl1 = [3]
+            bowl2 = [4]
+            expected_score = [7]

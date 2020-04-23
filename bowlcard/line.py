1f90d22e6b88abd8cbff3edb3fd425b18c29e74b
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 17:07:36 +0100

    we can score a frame

--- bowlcard/line.py
+++ bowlcard/line.py
@@ -1,4 +1,10 @@
 class Line:
 
-    def get_score(self):
-        return 0
+    def __init__(self):
+        self.__score = 0
+
+    def get_score(self) -> int:
+        return self.__score
+
+    def score_frame(self, bowl1: int, bowl2: int):
+        self.__score = self.__score + bowl1 + bowl2

3c684f0df6bc20511b0e688f213a7253f3fcbb09
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-23 18:04:03 +0100

    refactor to create the line in a setUp function
    note that the ddt table (or iterators) will call the given on
    each pass but will not call the setUp method

--- specs/line_spec.py
+++ specs/line_spec.py
@@ -4,27 +4,30 @@ from bowlcard.line import Line
 
 class LineSpec(Specification):
 
+    def setUp(self) -> None:
+        self.line = Line()
+
     def a_new_line_will_return_a_score_of_zero(self):
 
         with expect:
-            Line().get_score() == 0
+            self.line.get_score() == 0
 
 
     def we_can_score_multiple_frames(self):
 
         with given:
-            line = Line()
+            self.line = Line()
 
         with when:
-            line.score_frame(frame1_bowl1, frame1_bowl2)
-            line.score_frame(frame2_bowl1, frame2_bowl2)
+            self.line.score_frame(frame1_bowl1, frame1_bowl2)
+            self.line.score_frame(frame2_bowl1, frame2_bowl2)
 
         with then:
-            line.get_score() == expected_score
-            line.get_frame(1).bowl1 == frame1_bowl1
-            line.get_frame(1).bowl2 == frame1_bowl2
-            line.get_frame(2).bowl1 == frame2_bowl1
-            line.get_frame(2).bowl2 == frame2_bowl2
+            self.line.get_score() == expected_score
+            self.line.get_frame(1).bowl1 == frame1_bowl1
+            self.line.get_frame(1).bowl2 == frame1_bowl2
+            self.line.get_frame(2).bowl1 == frame2_bowl1
+            self.line.get_frame(2).bowl2 == frame2_bowl2
 
         with where:
             frame1_bowl1 | frame1_bowl2 | frame2_bowl1 | frame2_bowl2 | expected_score
